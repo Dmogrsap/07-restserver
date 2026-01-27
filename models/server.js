769af48a2a6a72f8741457822c2535010169e7d9
v68615 <dortega01@txtav.com>
2026-01-27 15:50:55 -0600

    app 07

---
+++ models/server.js
@@ -0,0 +1,30 @@
+
+import express from 'express';
+
+
+class Server {
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+
+        this.routes();
+
+    }
+
+    routes() {
+        this.app.get('/', (req, res) => {
+            res.send('Hello World');
+        });
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log('Server is running on PORT: ', this.port);
+        });
+    }
+
+}
+
+// module.exports = Server;
+export default Server;
