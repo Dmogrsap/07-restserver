232b66e33cc20a2726a85f2a5296dcd3e42dd943
v68615 <dortega01@txtav.com>
2026-03-03 15:58:02 -0600

    cambios en endpoints

--- models/server.js
+++ models/server.js
@@ -8,13 +8,54 @@ class Server {
         this.app = express();
         this.port = process.env.PORT;
 
+        //Middlewares
+        //this.app.use(express.static('public'));
+        this.middlewares();
+
         this.routes();
 
     }
 
+    middlewares() {
+        //Public Folder
+        this.app.use(express.static('public'));
+    }
+
+
     routes() {
-        this.app.get('/', (req, res) => {
-            res.send('Hello World');
+        this.app.get('/api', (req, res) => {
+            res.json({
+                ok: true,
+                msg: 'get API'
+            });
+        });
+
+        this.app.put('/api', (req, res) => {
+            res.json({
+                ok: true,
+                msg: 'put API'
+            });
+        });
+
+        this.app.post('/api', (req, res) => {
+            res.json({
+                ok: true,
+                msg: 'post API'
+            });
+        });
+
+        this.app.delete('/api', (req, res) => {
+            res.json({
+                ok: true,
+                msg: 'delete API'
+            });
+        });
+
+        this.app.patch('/api', (req, res) => {
+            res.json({
+                ok: true,
+                msg: 'patch API'
+            });
         });
     }
 
